184b9386e049420cb0113d0f8640c5ace4a4b880
rioalifian12 <rioalifian12@gmail.com>
2020-04-30 12:30:14 +0700

    index

---
+++ index.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <style>
+    section {
+      min-height: 420px;
+    }
+
+    .jumbotron {
+      position: relative;
+      background: #000 url("bg.jpg") center center;
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      overflow: hidden;
+    }
+  </style>
+
+  <title>Index Of Pemrograman Web</title>
+</head>
+
+<body class="mt-5">
+
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-info">
+    <div class="container">
+      <a class="navbar-brand" href="#">Rio AS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link" href="#top">Home<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#pw">Pemrograman Web</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container text-center">
+      <h1 class="display-4 text-white">Rio Alifian Santoso</h1>
+      <p class="lead text-white">This is my Website</p>
+    </div>
+  </div>
+
+  <section id="pw" class="about">
+    <div class="container">
+      <div class="row mb-4">
+        <div class="col text-center">
+          <h2>Pemrograman Web</h2>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-md-4 text-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Kuliah
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="kuliah/pertemuan9">Pertemuan9</a>
+              <a class="dropdown-item" href="kuliah/pertemuan10">Pertemuan10</a>
+              <a class="dropdown-item" href="kuliah/pertemuan11/index.php">Pertemuan11</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 text-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Praktikum
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="praktikum/P5_PW_193040057">P5_PW_193040057</a>
+              <a class="dropdown-item" href="praktikum/P6_PW_193040057">P6_PW_193040057</a>
+              <a class="dropdown-item" href="praktikum/P7_PW_193040057">P7_PW_193040057</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 text-center">
+          <div class="btn-group">
+            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Tugas Besar
+            </button>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Tubes</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class="bg-secondary text-white">
+    <div class="container">
+      <div class="row pt-3">
+        <div class="col text-center">
+          <p>Copyright &copy 2020</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
